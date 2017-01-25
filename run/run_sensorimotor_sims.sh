76912ad1e6dc6a59f8b19bc6fdfe1669975e4092
Francesco Mannella <Francesco Mannella>
2017-01-25 12:05:38 +0100

    runs

--- run/run_sensorimotor_sims.sh
+++ run/run_sensorimotor_sims.sh
@@ -1,19 +1,73 @@
 #!/bin/bash
 
-TIMESTEPS=$1
+#!/bin/bash
 
+set -e
+
+usage()
+{
+cat << EOF
+
+usage: $0 options
+
+This script runs batteries of robot simulations  
+
+OPTIONS:
+   -t --stime       number of timesteps of a single simulation block
+   -n --num         number of simulations
+   -h --help        show this help
+
+EOF
+}
+
+TIMESTEPS=200000
+ITER=1
+
+
+# getopt
+GOTEMP="$(getopt -o "t:n:h" -l "stime:,num:,help"  -n '' -- "$@")"
+
+if ! [ "$(echo -n $GOTEMP |sed -e"s/\-\-.*$//")" ]; then
+    usage; exit;
+fi
+
+eval set -- "$GOTEMP"
+
+
+while true ;
+do
+    case "$1" in
+        -t | --stime) 
+            TIMESTEPS="$2"
+            shift 2;;
+        -n | --num) 
+            ITER="$2"
+            shift 2;;
+        -h | --help)
+            echo "on help"
+            usage; exit;
+            shift;
+            break;;
+        --) shift ; 
+            break ;;
+    esac
+done
+
+cd ${HOME}
 if [ -z "$(mount | grep working)" ]; then 
-    sshfs -o reconnect,ServerAliveInterval=15,ServerAliveCountMax=3 rennes:/home/fmannella/working ${HOME}/working
+    [ -z "$(ls -A working/)" ] && \
+        sshfs -o reconnect,ServerAliveInterval=15,ServerAliveCountMax=3 rennes:/home/fmannella/working ${HOME}/working
 fi
 
 run()
 {
     CURR=$1
+    NUM=$2
     curr=$( echo $CURR| sed -e"s/\(.*\)/\L\1\E/")
 
     cd ${HOME}/working/sensorimotor-development
     
-    if [ -z ${curr}.tar.gz ]; then 
+    if [ -z ${curr}-${NUM}.tar.gz ]; then 
         perl -pi -e "s/^(\s*)([^#]+)( # MIXED)(\s*)$/\1# \2\3\n/" src/model/Robot.py 
         perl -pi -e "s/^(\s*)([^#]+)( # PRED)(\s*)$/\1# \2\3\n/" src/model/Robot.py 
         perl -pi -e "s/^(\s*)([^#]+)( # MATCH)(\s*)$/\1# \2\3\n/" src/model/Robot.py 
@@ -22,30 +76,29 @@ run()
 
         perl -pi -e "s/^(\s*)# ([^#]+)( # $CURR)(\s*)\n$/\1\2\3\n/" src/model/Robot.py 
 
-        run/run_batch.sh -t $TIMESTEPS
+        wdir=test-$curr-${NUM}
+
+        run/run_batch.sh -t $TIMESTEPS -w $wdir
         if [ $? -ne 0 ];then  
             for f in  $(find $(cat datadir)); 
             do
-                cp -r $f test/$(echo $(basename $f) | sed -e"s/_[0-9] $//"); 
+                cp -r $f ${wdir}/$(echo $(basename $f) | sed -e"s/_[0-9] $//"); 
             done
 
             R CMD BATCH rscripts/plot.R 
             if [ -f plot.pdf ]; then
-                mv plot.pdf test/${curr}.pdf  
-                tar czvf ${curr}.tar.gz test/
+                mv plot.pdf ${wdir}/${curr}.pdf  
+                tar czvf ${curr}-${NUM}.tar.gz test/
             fi
         fi
     fi
 }
 
-# MIXED
-run MIXED 
-run MIXED-2 
-
-# PRED
-run PRED
-
-# MATCH
-run MATCH
-run MATCH-2
+for num in $(seq $ITER); 
+do
+    run MIXED $num & run MIXED-2 $num & run PRED $num &
+    wait
+    run MATCH $num & run MATCH-2 $num &
+    wait
+done
 
