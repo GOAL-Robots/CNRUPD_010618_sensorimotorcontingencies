bea14301457deafb572406835067e08cd9e42e53
Francesco Mannella <Francesco Mannella>
2017-04-09 10:23:22 +0200

    changed g5k.sh

--- run/g5k.sh
+++ run/g5k.sh
@@ -1,13 +1,42 @@
 #!/bin/bash
 
 source ${HOME}/g5kutils/setwalltime.sh
+${HOME}/g5kutils/clear.sh
+[ ! -d ${HOME}/.sensorimotor ] && mkdir ${HOME}/.sensorimotor
 
 IFS=$'\n'
 N_MACHINES=2
 MIN_CORES=30
 MIN_RAM=100
 WALLTIME=$(max_walltime)
+declare -a dirs=(sm_singleecho_25g)
+declare -a params
 
+params[0]=$(cat<<HERE_PARAMS
+simulation_competence_improvement_prop = 1.0
+simulation_incompetence_prop = 0.0
+gs_n_echo_units = 800
+gs_multiple_echo = False
+GOAL_NUMBER = 25
+HERE_PARAMS
+)
+
+# CLEAR NON RUNNING SIMULATIOMS
+running_jobs="$(basename -a $(find .G_*)|sed -e"s/\.G_//")"
+for job in $(ls ${HOME}/.sensorimotor/*); do
+    if [ -z "$(echo "$running_jobs" | grep "\<$job\>")" ]; then
+        rm -fr ${HOME}/.sensorimotor/$job
+    else
+        for sim_info_dir in ${dirs[@]}; do
+            if [ ! -z "$(cat  ${HOME}/.sensorimotor/$job|grep $sim_info_dir)" ]; then 
+                echo "simulation already running!"
+                exit 1
+            fi
+        done
+    fi
+done
+
+# FIND RESOURCES 
 cluster=$(${HOME}/g5kutils/select_res.sh -c $MIN_CORES -n $N_MACHINES -r $MIN_RAM -t $WALLTIME | grep resource: |  head -n 1 | awk '{print $3}')
 
 [ -z "$cluster" ] && echo "no reservation avaliable" && exit 1
@@ -27,20 +56,11 @@ if [ -z "$JOB_ID" ]; then
 fi
 
 declare -a nodes=($(cat ~/.G_${JOB_ID}/NODES|uniq))
-declare -a dirs=(sm_singleecho_25g)
 
-params[0]=$(cat<<HERE_PARAMS
-simulation_competence_improvement_prop = 1.0
-simulation_incompetence_prop = 0.0
-gs_n_echo_units = 800
-gs_multiple_echo = False
-GOAL_NUMBER = 25
-HERE_PARAMS
-)
+# SAVE SIMULATION STATUS IN INFO DIR 
+echo "${dirs[@]}" > ${HOME}/.sensorimotor/$JOB_ID 
 
-
-echo $params
-
+# START SIMULATIONS
 for i in $(seq 0 0); do
     individual=1
     for node in ${nodes[@]}; do
