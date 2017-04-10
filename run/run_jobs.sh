5f36352a26c31041a9115d294067e910fdca6033
Francesco Mannella <Francesco Mannella>
2017-04-10 13:43:50 +0200

    new

--- run/run_jobs.sh
+++ run/run_jobs.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-SCREEN=/usr/bin/screen
-
-# control path variable in screen
-[ ! -e ${HOME}/.screenrc ] && echo "setenv PATH $PATH" > ${HOME}/.screenrc
 
 # control for the existence of a correct screen session
 screen_session="$(${SCREEN} -ls | grep "\<sm\>"| awk '{print $1}')"
@@ -11,16 +7,20 @@ screen_session="$(${SCREEN} -ls | grep "\<sm\>"| awk '{print $1}')"
    ${SCREEN} -dmS sm
 
 # clean previous windows
-WINS="$(screen -S sm -Q windows |\
-    sed -e"s/\(\(\s\+\)*\)\([0-9]\+\s\+\)/\n/g; s/^\n//g"|head -n -2)"
-for win_id in $(cat WINS| sed -e"s/^\([0-9]\+\)\s\+.*/\1/"); do
-    ${screen} -S sm -p $win_id -X kill
+WINS="$(screen -S sm -Q windows |sed -e"s/\s\+\([0-9]\)/\n\1/g")"
+
+
+for win_id in $(echo "$WINS"| sed -e"s/^\([0-9]\+\)\s\+.*/\1/"); do
+    echo $win_id
+    eval "screen -S sm -p $win_id -X kill"
 done
 
+
 screen_window="sm_$(date +"%Y%m%d%H%M")"
-${SCREEN} -S sm -p ${screen_window}  -X screen
+
+screen -S sm -p ${screen_window}  -X screen
 
 # run command in the current window
-${SCREEN} -S sm -p ${screen_window} \
+screen -S sm -p ${screen_window} \
     -X stuff "/home/fmannella/working/sensorimotor-development/run/g5k.sh &> ${HOME}/log_jobs\n"
 
