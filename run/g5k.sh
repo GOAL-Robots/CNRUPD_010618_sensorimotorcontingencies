a95e068c9e2a661f7a15842aa0f0110b4f7e14f9
Francesco Mannella <Francesco Mannella>
2017-04-19 17:15:41 +0200

    g5k.sh

--- run/g5k.sh
+++ run/g5k.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
 set -e
+set -o pipefail
 IFS=$'\n'
 
 source ${HOME}/g5kutils/setwalltime.sh
 ${HOME}/g5kutils/clear.sh
 [ ! -d ${HOME}/.sensorimotor ] && mkdir ${HOME}/.sensorimotor
 
-N_MACHINES=2
-MIN_CORES=30
+N_MACHINES=4
+MIN_CORES=20
 MIN_RAM=100
 WALLTIME=$(max_walltime)
 declare -a dirs=(sm_singleecho_25g)
@@ -23,47 +24,54 @@ GOAL_NUMBER = 25
 HERE_PARAMS
 )
 
-# CLEAR NON RUNNING SIMULATIOMS
-echo "clear non-running simulations"
-running_jobs=
-[ ! -z "$(ls -a ${HOME} | grep "\.G_" )" ] &&
-    running_jobs="$(basename -a $(ls -a ${HOME}| grep "\.G_")|sed -e"s/\.G_//")"
-for job in $(ls ${HOME}/.sensorimotor/ | grep "[0-9]\+"); do
-    if [ -z "$(echo "$running_jobs" | grep "\<$(basename $job)\>")" ]; then
-        rm -fr $job
-    else
-        for sim_info_dir in ${dirs[@]}; do
-            if [ ! -z "$(cat  $job|grep $sim_info_dir)" ]; then 
-                echo "simulation already running!"
-                exit 1
-            fi
-        done
-    fi
-done
-
 # FIND RESOURCES 
-cluster=$(${HOME}/g5kutils/select_res.sh -c $MIN_CORES -n $N_MACHINES -r $MIN_RAM -t $WALLTIME | grep resource: | tail -n 1 | awk '{print $3}')
+${HOME}/g5kutils/find_resources.sh -c $MIN_CORES -n $N_MACHINES -r $MIN_RAM -w $WALLTIME 2>&1 | tee log_resources
 
-[ -z "$cluster" ] && echo "no reservation avaliable" && exit 1
+# exit if no resources 
+[ $? -ne 0 ] && exit 1 
 
-echo
-echo "deployng $N_MACHINES machines on $cluster:"
-echo
-echo
+# get job_id
+JOB_ID=$(cat log_resources| grep JOB_ID| sed -e"s/.*:\s*\([0-9]\+\)\s*$/\1/")
+# save it in log dir
+echo -n ''> ${HOME}/.sensorimotor/$JOB_ID
 
-${HOME}/g5kutils/deploy.sh -t $WALLTIME -u $cluster -n $N_MACHINES -f | tee log_deploy 2>&1
-
-JOB_ID="$(cat log_deploy|grep JOB_ID| sed  -e"s/.*:\s\+\([0-9]\+\)\s*$/\1/")"
-
-if [ -z "$JOB_ID" ]; then
-    echo "no reservation avaliable"
-    exit 1
-fi
-
+# set array of node names
 declare -a nodes=($(cat ~/.G_${JOB_ID}/NODES|uniq))
 
-# SAVE SIMULATION STATUS IN INFO DIR 
+# save simulation status in log dir 
 echo "${dirs[@]}" > ${HOME}/.sensorimotor/$JOB_ID 
+
+
+
+run_cmd()
+{
+
+wdir=$1
+node=$2
+
+# make run file and store it within the working dir 
+cat << EOS > ${HOME}/working/${wdir}/run
+cd ~/working/$wdir
+\${HOME}/working/sensorimotor-development/run/g5k_batteries.sh -t 50000 -n 1000 -b -P params
+EOS
+
+# build the command to run in the machine
+CMD=$(cat<<EOS
+[ -z "\$(mount | grep working )" ] && \
+    (sshfs -o reconnect,ServerAliveInterval=15,ServerAliveCountMax=3 \
+    rennes:\${HOME}/working \${HOME}/working)
+
+screen -dmS $(basename $wdir)
+screen -S $(basename $wdir) -X exec bash -c "\${HOME}/working/$wdir/run; bash"
+EOS
+)
+
+# execute the command within the machine
+ssh $node "$CMD"
+
+}
+
+
 
 # START SIMULATIONS
 for i in $(seq 0 0); do
@@ -81,29 +89,11 @@ for i in $(seq 0 0); do
 
         echo "${params[i]}" > ${HOME}/working/${wdir}/params
 
-#-HERE---------------------------------------------------------------------
-cat << EOS > ${HOME}/working/${wdir}/run
-cd ~/working/$wdir
-\${HOME}/working/sensorimotor-development/run/g5k_batteries.sh -t 50000 \\
--n 1000 -b -P params
-EOS
-#-HERE-END-----------------------------------------------------------------
-
         chmod +x ${HOME}/working/${wdir}/run
 
-#-HERE---------------------------------------------------------------------
-CMD=$(cat<<EOS
-[ -z "\$(mount | grep working )" ] && \
-    (sshfs -o reconnect,ServerAliveInterval=15,ServerAliveCountMax=3 \
-    rennes:\${HOME}/working \${HOME}/working)
+        run_cmd $wdir $node
 
-screen -dmS $(basename $wdir)
-screen -S $(basename $wdir) -X exec bash -c "\${HOME}/working/$wdir/run; bash"
-EOS
-)
-#-HERE-END-----------------------------------------------------------------o
 
-        ssh $node "$CMD"
         individual=$((individual+1))
 
     done
--- run/run_jobs.sh
+++ run/run_jobs.sh
@@ -1,9 +1,32 @@
 #!/bin/bash
 
+
+# CLEAR NON RUNNING SIMULATIOMS
+clear_simulations()
+{
+    echo "clear non-running simulations"
+    running_jobs=
+    [ ! -z "$(ls -a ${HOME} | grep "\.G_" )" ] &&
+        running_jobs="$(basename -a $(ls -a ${HOME}| grep "\.G_")|sed -e"s/\.G_//")"
+    for job in $(ls ${HOME}/.sensorimotor/ | grep "[0-9]\+"); do
+        if [ -z "$(echo "$running_jobs" | grep "\<$(basename $job)\>")" ]; then
+            rm -fr $job
+        else
+            for sim_info_dir in ${dirs[@]}; do
+                if [ ! -z "$(cat  $job|grep $sim_info_dir)" ]; then 
+                    echo "simulation already running!"
+                    exit 1
+                fi
+            done
+        fi
+    done
+}
+
 SCREEN=/usr/bin/screen
 
 LOG_DIR=${HOME}/.sensorimotor
 [ ! -d $LOG_DIR ] && mkdir $LOG_DIR
+clear_simulations &> ${LOG_DIR}/clear_log
 
 # control for the existence of a correct session
 screen_session="$(${SCREEN} -ls | grep "\<sm\>"| awk '{print $1}')"
