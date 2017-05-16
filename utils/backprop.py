47950257d21144b578be0e55c5535d5048ae4096
Francesco Mannella <franc3sco.mannella@gmail.com>
2017-05-16 09:52:19 +0000

    xor

--- utils/backprop.py
+++ utils/backprop.py
@@ -123,17 +123,18 @@ class BackProp:
         # the initial outer-error is the real one
         # (expected vs real outputs)
         
-		self.error = target - self.units[-1]
-		self.deltas[-1] = self.error
+        self.error = target - self.units[-1]
 
+        self.deltas[-1] = self.error * \
+                self.derfun(self.units[-1])
         # iterate layers backward
         for layer in  xrange(self.n_layers-1, 0, -1) :
             # for each layer evaluate the
             # back-propagation error
-            w_T = (self.weights[layer-1][:,1:]).T
-            self.deltas[layer-1] = \
-				np.dot( w_T, self.deltas[layer] ) * \
-				self.derfun(layer - 1)
+            w_T = (self.weights[layer-1][:, 1:]).T
+            self.deltas[layer - 1] = \
+                    np.dot(w_T, self.deltas[layer]) * \
+                    self.derfun(self.units[layer - 1])
 
         #---------------------------------------------------------------------
         # weight updates
@@ -141,31 +142,56 @@ class BackProp:
         for layer in  xrange(self.n_layers-1):
             # error plus derivative
             self.weights[layer] += self.eta * np.outer(               
-                    self.deltas[layer + 1] *
-					self.derfun(self.units[layer + 1]),
+                    self.deltas[layer + 1],
                     biased(self.units[layer]))
 
     def get_mse(self):
-        return 0.5*(np.mean(self.error*2))
+        return 0.5*(np.mean(self.error**2))
 
 
 # ----------------------------------------------------------------------------
 # ----------------------------------------------------------------------------
 # test
+def xor_test():
 
-if __name__ == "__main__":
+    bp = BackProp(
+        outfun = sigmfun, derfun = sigmder,
+        n_units_per_layer = [2,  2, 1],
+        eta = 1)
+
+    data =np.array([
+            [0, 0, 0],
+            [1, 0, 1],
+            [0, 1, 1],
+            [1, 1, 0]])
+    idcs = range(4)
+    trials = 20000
+
+    for t in xrange(trials):
+        random.shuffle(idcs)
+        errors = np.zeros(4)
+        for p in idcs:
+            bp.step(data[p,:2])
+            bp.learn(data[p,2])
+            errors[p] = bp.get_mse()
+            print data[p], bp.units[-1], errors[p]
+        print
+        print np.mean(errors)
+        print
+
+def mnist_test():
 
     np.set_printoptions(suppress = True,
                         precision = 2,
                         linewidth = 10000)
 
-    import matplotlib.pyplot as plt
-    plt.ion()
-    fig = plt.figure()
-    ax = fig.add_subplot(111)
-    line, = ax.plot(0)
-    ax.set_xlim([-10, 3e2+10])
-    ax.set_ylim([0, 1.2e-1])
+    # import matplotlib.pyplot as plt
+    # plt.ion()
+    # fig = plt.figure()
+    # ax = fig.add_subplot(111)
+    # line, = ax.plot(0)
+    # ax.set_xlim([-10, 3e2+10])
+    # ax.set_ylim([0, 1.2e-1])
 
     # import the mnist class
     from mnist import MNIST
@@ -211,6 +237,11 @@ if __name__ == "__main__":
             errors[t] = bp.get_mse()
 
         errsums[k] = np.mean(errors[rng]) 
-        line.set_data(10+np.arange(k), errsums[:k])
         print k, errsums[k]
-        plt.pause(0.01)
+        # line.set_data(10+np.arange(k), errsums[:k])
+        # plt.pause(0.01)
+
+if __name__ == "__main__":
+
+    xor_test()
+
