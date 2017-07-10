f57b898c993d535c6dd2bd4210ed50e341bbc5de
Francesco Mannella <Francesco Mannella>
2017-07-10 11:46:39 +0200

    g5k

--- run/clear_simulations.sh
+++ run/clear_simulations.sh
@@ -1,19 +1,32 @@
 #!/bin/bash
 
+GRID_INFO=${HOME}/.grid_deploy/info
+
 
 # CLEAR NON RUNNING SIMULATIOMS
 echo "clear non-running simulations"
+
 running_jobs=
-[ ! -z "$(ls -a ${HOME} | grep "\.G_" )" ] &&
-    running_jobs="$(basename -a $(ls -a ${HOME}| grep "\.G_")|sed -e"s/\.G_//")"
-echo "running_jobs: $running_jobs"
-for job in $(ls ${HOME}/.sensorimotor/ | grep "[0-9]\+"); do
-    if [ -z "$(echo "$running_jobs" | grep "\<$(basename $job)\>")" ]; then
+[ ! -z "$(cat $GRID_INFO)" ] && \
+    running_jobs="$(cat $GRID_INFO|grep sensorimotor|awk '{print $2}')"
+
+if [ -z $running_jobs ]; then
+
+    for job in $(ls ${HOME}/.sensorimotor/ | grep "[0-9]\+"); do
         echo "cleared job $job"
         rm -fr $job
-    else
-        echo "simulation already running!"
-        exit 1
-    fi
-done
+    done
+    
+else
+
+    for job in $(ls ${HOME}/.sensorimotor/ | grep "[0-9]\+"); do
+        if [ -z "$(echo "$running_jobs" | grep "\<$(basename $job)\>")" ]; then
+            echo "cleared job $job"
+            rm -fr $job
+        fi
+    done
+
+    echo "simulation already running!"
+    exit 1
+fi
 
