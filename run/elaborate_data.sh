f01c43c5b62ed34df1a6fb7259afeb58c1be7152
Francesco Mannella <francesco.mannella@gmail.com>
2017-07-14 08:07:50 +0200

    final_graphs

--- run/elaborate_data.sh
+++ run/elaborate_data.sh
@@ -85,7 +85,7 @@ rm -fr $TMP_DIR/*
 echo "data dir: $DIR"
 echo "source dir: $BASE"
 
-if [ $GRAPHS == true ]; then
+if [ $GRAPHS == true ] && [ $WWW == true ]; then
 
 cat << EOF > $TMP_DIR/plots.html
 <!DOCTYPE html>
