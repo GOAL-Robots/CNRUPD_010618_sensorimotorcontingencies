5ed794621893e804fba1c10a5e6d365a885bd5f3
Francesco Mannella <francesco.mannella@gmail.com>
2017-11-30 08:50:53 +0100

    bp

--- utils/backprop_modulated.py
+++ utils/backprop_modulated.py
@@ -6,6 +6,7 @@ np.set_printoptions(suppress=True,
                     precision=5,
                     linewidth=10000)
 
+#------------------------------------------------------------------------------ 
 
 class Predictor(object):
     pass
@@ -18,27 +19,26 @@ class BackPropMod(BackProp):
             of correctness and modulates learning.
         """
         super(BackPropMod, self).__init__(*args, **kargs)
-
         
     def learn(self, target):
         """
         :param  target  the desired output for the current input
         """
-
-
         self.error_backprop(target)
         self.update_weights()
         
-        
-    
-n_mnist_pixels = 28*28
+#------------------------------------------------------------------------------ 
 
-pred = Predictor()
-# init backprop object
-bp = BackPropMod(
-    pred=pred, outfun=sigmfun, derfun=sigmder,
-    n_units_per_layer=[n_mnist_pixels,
-                       100, 10, 100,
-                       n_mnist_pixels],
-    eta=0.01)
-print "Done"
+if __name__ == "__main__":
+
+    n_mnist_pixels = 28*28
+    
+    pred = Predictor()
+    # init backprop object
+    bp = BackPropMod(
+        pred=pred, outfun=sigmfun, derfun=sigmder,
+        n_units_per_layer=[n_mnist_pixels,
+                           100, 10, 100,
+                           n_mnist_pixels],
+        eta=0.01)
+    print "Done"
